3e14654b91f4af044251867468fc86e11161e7ad
Erin Sheldon <erin.sheldon@gmail.com>
2013-12-16 15:41:57 -0500

    got writing of control files working

--- gmix_meds/dbload.py
+++ gmix_meds/dbload.py
@@ -1,67 +1,137 @@
+import os
 
-def make_tile_input(run, tilename, ftype, blind=True, create=False):
+def make_all_oracle_input(run, ftype, table_name, blind=True):
     """
-    Create the control file and potentially the create table file
+    Make inputs for all tiles used in the specified run
     """
     import desdb
     import deswl
-    import fitsio
-    if blind:
-        out_ftype='wlpipe_me_collated_blinded'
-    else:
-        out_ftype='wlpipe_me_collated'
+
+    tilenames = get_tilenames(run)
+    ntiles=len(tilenames)
+    print 'found',ntiles,'tiles'
+    for i,tilename in enumerate(tilenames):
+        print '%03d/%03d %s' % (i+1,ntiles,tilename)
+
+        create=(i==0)
+        make_oracle_input(run, tilename, ftype, table_name,
+                          blind=blind, create=create)
+
+def make_oracle_input(run, tilename, ftype, table_name, blind=True, create=False):
+    """
+    convenience function to create the oracle input
+    """
+    oim=OracleInputMaker(run, tilename, ftype, table_name,
+                         blind=blind, create=create)
+
+    oim.make_tile_input()
+
+class OracleInputMaker(object):
+    """
+    Create the input csv and control file.  Potentially make the sql statement
+    for table creation.
+    """
+    def __init__(self, run, tilename, ftype, table_name, blind=True, create=False):
+        self.run=run
+        self.tilename=tilename
+        self.ftype=ftype
+
+        self.table_name=table_name
+
+        self.blind=blind
+        self.create=create
+
+        self.set_info()
 
 
-    rc=deswl.files.Runconfig(run)
-    bands=rc['band']
-    band_cols=get_band_cols()
+    def make_tile_input(self):
+        """
+        Create the control file and potentially the create table file
+        """
+        import desdb
+        import fitsio
 
-    df=desdb.files.DESFiles()
-    fname = df.url(out_ftype,
-                   run=run,
-                   tilename=tilename,
-                   filetype=ftype,
-                   ext='fits')
+        with fitsio.FITS(self.fname) as fobj:
+            model_fits=fobj['model_fits'][:]
+            epoch_data=fobj['epoch_data'][:]
 
-    with fitsio.FITS(fname) as fobj:
-        model_fits=fobj['model_fits'][:]
-        epoch_data=fobj['epoch_data'][:]
+        if not os.path.exists(self.outdir):
+            os.makedirs(self.outdir)
+
+        desdb.array2table(model_fits,
+                          self.table_name,
+                          self.control_file,
+                          bands=self.bands,
+                          band_cols=self.band_cols,
+                          create=self.create,
+                          primary_key=self.primary_key)
+
+        desdb.array2table(epoch_data,
+                          self.epochs_table_name,
+                          self.epochs_control_file,
+                          create=self.create)
+
+    def set_info(self):
+        """
+        Set some info needed to do our work
+        """
+        import desdb
+        import deswl
+
+        self.epochs_table_name = get_epochs_table_name(self.table_name)
+
+        if self.blind:
+            out_ftype='wlpipe_me_collated_blinded'
+        else:
+            out_ftype='wlpipe_me_collated'
+
+        self.rc=deswl.files.Runconfig(self.run)
+
+        self.df=desdb.files.DESFiles()
+        self.fname = self.df.url(out_ftype,
+                                 run=self.run,
+                                 tilename=self.tilename,
+                                 filetype=self.ftype,
+                                 ext='fits')
+
+        dirname=os.path.dirname(self.fname)
+        bname=os.path.basename(self.fname)
+
+        self.outdir=os.path.join( dirname, 'oracle' )
+
+        control_file=os.path.basename(bname).replace('.fits','.ctl')
+        self.control_file=os.path.join(self.outdir, control_file)
+
+        self.epochs_control_file=self.control_file.replace('.ctl','-epochs.ctl')
 
 
-    control_file=fname.replace('.fits','.ctl')
-    epoch_control_file=fname.replace('.fits','-epoch.ctl')
+        self.bands=self.rc['band']
+        self.band_cols=get_band_cols()
 
-    table, epoch_table = get_table_names(run)
+        self.primary_key='coadd_objects_id'
 
-    desdb.array2table(model_fits, table, control_file,
-                      bands=bands, band_cols=band_cols,
-                      create=create,
-                      primary_key='coadd_objects_id')
-    desdb.array2table(epoch_data, epoch_table, epoch_control_file,
-                      create=create)
-
-def add_indexes(run):
+def add_indexes(table_name):
     """
     Add indexes to the appropriate columns
     """
     import desdb
-    table, epoch_table = get_table_names(run)
+    epochs_table_name = get_epochs_table_name(table_name)
 
     index_cols=get_index_cols()
     epoch_index_cols=get_epoch_index_cols()
 
-    qt="create index {col}_idx on {table}({col})"
+    qt="create index {col}_idx on {table_name}({col})"
 
     conn=desdb.Connection()
     curs = conn.cursor()
     for col in index_cols:
-        query = qt.format(table=table, col=col)
+        query = qt.format(table_name=table_name, col=col)
         print query
 
         curs.execute(query)
 
     for col in epoch_index_cols:
-        query = qt.format(table=epoch_table, col=col)
+        query = qt.format(table_name=epochs_table_name, col=col)
         print query
 
         curs.execute(query)
@@ -69,13 +139,12 @@ def add_indexes(run):
     curs.close()
     conn.close()
 
-def get_table_names(run):
+def get_epochs_table_name(table_name):
     """
-    Table names based on run
+    For a table get the corresponding epochs
+    table name
     """
-    table=run
-    epoch_table='%s_epochs' % run
-    return table, epoch_table
+    return '%s_epochs' % table_name
 
 def get_band_cols():
     colnames=['nimage_tot',
@@ -132,3 +201,24 @@ def get_epoch_index_cols():
             'band',
             'band_num',
             'psf_fit_flags']
+
+def get_tilenames(run):
+    """
+    Get all associated tilenames
+    """
+    import desdb
+    import deswl
+
+    rc=deswl.files.Runconfig(run)
+    releases=rc['dataset']
+    bands=rc['band']
+
+    info_list=desdb.files.get_coadd_info_by_release(releases,'g',
+                                                    withbands=bands)
+
+    tiles = [info['tilename'] for info in info_list]
+    tiles.sort()
+
+    return tiles
+
+
