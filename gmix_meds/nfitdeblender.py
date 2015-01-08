57e0c838fc85d6e6b7b93e852554483a8b8f93da
Matthew R Becker <becker.mr@gmail.com>
2015-01-08 15:41:31 -0800

    partial commit of code that is not complete - stopping to do a partial refactor into just processing generalized FoFs

--- gmix_meds/nfitdeblender.py
+++ gmix_meds/nfitdeblender.py
@@ -27,12 +27,80 @@ class MLDeblender(MedsFit):
                                          model_data=model_data,
                                          checkpoint_file=checkpoint_file,
                                          checkpoint_data=checkpoint_data)
-        self._build_fofmems_hash()
+
+    """
+    Indexing notes:
+    
+        self.dindex = 0-offset index into number of objects currently being processed
+            This also indexes the output data table, self.data.
+        self.mindex = self.index_list[dindex] 
+            = 0-offset index into input meds file
+        
+    Note that mindex and dindex can differ if a finite range of objects is begin processed.
+    Also, if a subset of a meds file is passed in, mindex is an index into the subset of 
+    the meds file. 
+    
+    Note that in the case of deblending, we never use a subset the range of objects in the file using
+    self.obj_range. We do use subsets with the fof_range.
+    
+    The FoFs are made with the global seg map IDs. These are usually called "number" and are 
+    1-offset. The mindex above equals number-1, if a total MEDs file is being processed. However 
+    for subsets, the last statement is not true. Instead then mindex is for the local MEDS file only. 
+    
+    Below, we build lookup tables to enable easy translation. They are
+    
+        self.fofid2dindex = this is a dict keyed on the fofid - it returns the set of dindexes
+            that gives the members of the FoF group
+        self.fofid2number = a dict keyed by fofid that returns all the seg map IDs of a given FoF
+        self.dindex2fofid = a lookup table that returns the fofid of a given object
+    
+    If one loops through the fofids from 0 the Nfofs-1, you get back all dindexes in order and can 
+    use them in the self.index_list to get the mindexes.
+        
+        for fofid in fofids:
+            fof_dindexes = self.fofid2dindex[fofid] # these index the self.data table
+            fof_mindexes = self.index_list[fof_dindexes] # these subscript the local MEDS file
+            
+    """
+
+    def _set_index_list(self):
+        """
+        set the list of objects to be processed from the fof groups
+        """
+        if self.fof_range is None:
+            start_fof = 0
+            end_fof = len(self.mode_data['fofs']['fofid'])-1
+        else:
+            start_fof = self.fof_range[0]
+            end_fof = self.fof_range[1]
+            
+        #build the fof hash which sets the dindexes            
+        self.fofid2dindex = {}
+        self.fofid2number = {}
+        self.dindex2fofid = []
+        loc = 0
+        segnumbers = []
+        for fofid in range(start_fof,end_fof+1):
+            q, = np.where(self.model_data['fofs']['fofid'] == fofid)
+            assert len(q) > 0, print 'Found zero length FoF! fofid = %ld' % fofid
+            segnumbers.extend(list(self.model_data['fofs']['number'][q]))
+            
+            self.fofid2dindex[fofid] = np.arange(loc,loc+len(q))
+            self.fofid2number[fofid] = self.model_data['fofs']['number'][q]
+            self.dindex2fofid.extend(list([fofid for i in xrange(len(q))]))            
+            loc += len(q)
+        segnumbers = np.array(segnumbers,dtype=int)
+        self.dindex2fofid = np.array(self.dindex2fofid,dtype=int)
+            
+        #now set self.index_list to point the dindex-th element 
+        #with segnumbers[dindex] to the proper mindex in local meds file
         
         
-    def _build_fofmems_hash(self):
-        #FIXME: fill this in!
-        pass
+            
+        #double check against the fofid2number hash above against internal 
+        #seg ids in MEDS
+        
+        
         
     def do_fits(self):
         """
@@ -49,6 +117,7 @@ class MLDeblender(MedsFit):
         t0=time.time()
         
         #FIXME sort by size or mag
+        #FIXME do in order of fofs
         for dindex in xrange(num):
             if self.data['processed'][dindex] > 0:
                 continue
@@ -152,14 +221,14 @@ class MLDeblender(MedsFit):
         sets .fitter or .coadd_fitter
         """
         if coadd:
-            if self.data['processed'][dindex] == -1:
+            if self.data['processed'][dindex] == 0:
                 self.guesser = self._get_guesser('coadd_psf')
             else:
                 pars = self.data['coadd_'+model+'_pars_best'][dindex]
                 self.guesser = FixedParsGuesser(pars,pars*0.1)
             mb_obs_list=self.sdata['coadd_mb_obs_list']
         else:
-            if self.data['processed'][dindex] == -1:
+            if self.data['processed'][dindex] == 0:
                 self.guesser = self._get_guesser('me_psf')
             else:
                 pars = self.data[model+'pars_best'][dindex]
@@ -193,24 +262,6 @@ class MLDeblender(MedsFit):
 
         return fitter
         
-    def _set_index_list(self):
-        """
-        set the list of objects to be processed from the fof groups
-        """
-        if self.fof_range is None:
-            start=0
-            end=self.nobj_tot-1
-            self.index_list = numpy.arange(start,end+1)
-        else:
-            start=self.fof_range[0]
-            end=self.fof_range[1]
-            self.index_list = []
-            for fofid in range(start,end+1):
-                q, = np.where(self.model_data['fofs']['fofid'] == fofid)
-                assert len(q) > 0, print 'Found zero length FoF! fofid = %ld' % fofid
-                self.index_list.extend(list(self.model_data['fofs']['number'][q]-1))
-            self.index_list = np.array(self.index_list)
-                
     def _fit_simple_max(self, mb_obs_list, model, nm_pars, ntry=1):
         """
         parameters
